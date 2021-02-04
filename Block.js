543fa168cb487ba75ff10db7027c996a85b74855
Neel123GitHub <75197521+Neel123GitHub@users.noreply.github.com>
2021-02-04 17:46:26 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,27 @@
+class Block{
+    constructor(x,y,width,height){
+        var options={
+            friction:7,
+            density:3.8,
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width;
+        this.height=height;
+        this.Visiblity=255;
+        World.add(world,this.body);
+    }
+    display(){
+        if(this.body.speed<5){
+        var pos=this.body.position
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+        }
+        else{
+            World.remove(world,this.body);
+            push();
+            this.Visiblity=this.Visiblity-5;
+            tint(255,this.Visiblity);
+            pop();
+        }
+    }
+}
